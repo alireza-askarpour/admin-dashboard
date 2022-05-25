3cb0c497b5d2fe6a4f0e3ac965ebd4ad23ea5d96
Alireza Askarpour <alirezaaskarpourr@gmail.com>
2022-05-25 23:55:08 +0430

    feat: add trending up icon

---
+++ src/assets/icons/TrendingUp.tsx
@@ -0,0 +1,10 @@
+const TrendingUp = () => {
+  return (
+    <>
+      <path d="M23 6L13.5 15.5 8.5 10.5 1 18"></path>
+      <path d="M17 6L23 6 23 12"></path>
+    </>
+  )
+}
+
+export default TrendingUp
